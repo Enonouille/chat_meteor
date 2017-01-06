424c76f587877e60c925598233b21dc83d580fb6
Enora Gathignol <enora.gathignol@orange.fr>
2017-01-06 17:55:09 +0100

    [Frontend Renan] Try to fix bug

--- client/main.js
+++ client/main.js
@@ -7,45 +7,25 @@ Template.messages.helpers({
   messages: Messages.find({})
 });
 
-// Template.messages.helpers({
-//   messages: [
-//     {text: "All these messages"},
-//     {text: "Uses the same template"},
-//     {text: "But have a different data context"},
-//     {text: "It's why these messages are all different!"},
-//     {text: "Hello man"}
-//   ]
-// });
-
 
 console.log(Messages.find().fetch());
 console.log(Messages.find({}));
 
 
 Template.footer.events({
-  'keypress input': function(e) {
-    if (event.charCode == 13) {
+  'click .footer__box_btn': function(e) {
       const valueMessage = $('.input-box_text').val();
 
-      e.preventDefault();
+//      e.preventDefault();
+      e.stopPropagation();
+      Messages.insert({text: valueMessage}, function() {
+        console.log('insert');
+      });
+      
+      console.log(Messages.find().fetch());
+      
       $('.message-history').append(`<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">Renan</a><span class="message_timestamp">11:23 am</span><span class="message_star">Hello</span><span class="message_content">${valueMessage}</span></div>`);
       $('.input-box_text').val('');
-    } 
+      return false;
   }
 });
-
-
-// Template.footer.events({
-//   'keypress input': function(e) {
-//     var inputVal = $('.input-box_text').val();
-//     if(!!inputVal) {
-//       var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
-//       if (charCode == 13) {
-//         e.stopPropagation();
-//         Messages.insert({text: $('.input-box_text').val()});
-//         $('.input-box_text').val("");
-//         return false;
-//       }    
-//     }
-//   }
-// });
--- collections/messages.js
+++ collections/messages.js
@@ -1,9 +1,9 @@
 Messages = new Mongo.Collection("messages");
 
-Messages.insert({text: "hello"}, function() {
-  console.log('insert');
-});
-
-Messages.insert({text: "ploip"}, function() {
-  console.log('insert');
-});
+//Messages.insert({text: "hello"}, function() {
+//  console.log('insert');
+//});
+//
+//Messages.insert({text: "ploip"}, function() {
+//  console.log('insert');
+//});
