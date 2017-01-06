d7b9dee22b09a61d607d1e546946ee901d742727
Enora Gathignol <enora.gathignol@orange.fr>
2017-01-06 16:59:25 +0100

    [Frontend] Fix asideBar and footer textarea

---
+++ client/js/chatroomAside.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('.chatRoom_btn').on('click', function() {
+    $('.content').toggleClass('isOpen');
+  });
+});
--- client/main.js
+++ client/main.js
@@ -24,3 +24,5 @@ Template.footer.events({
     } 
   }
 });
+
+
